b8dca513775f5b0ba0c769305fed2a0b9764460c
kfamillaran74-art <kfamillaran74@gmail.com>
2026-09-05 19:45:05 +0800

    added splash screen

---
+++ app/src/main/java/com/example/myfirsttry/ui/SplashActivity.kt
@@ -0,0 +1,52 @@
+package com.example.myfirsttry
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.myfirsttry.ui.theme.MyFirstTryTheme
+import kotlinx.coroutines.delay
+import androidx.compose.runtime.LaunchedEffect
+
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MyFirstTryTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.splash_screen),
+                        contentDescription = "Cyberity Splash Screen",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                LaunchedEffect(Unit) {
+                    delay(2000)
+
+                    startActivity(
+                        Intent(
+                            this@SplashActivity,
+                            MainActivity::class.java
+                        )
+                    )
+
+                    finish()
+                }
+            }
+        }
+    }
+}
